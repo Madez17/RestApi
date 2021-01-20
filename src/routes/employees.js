cbd857a385133f76c489d80f0770cc3c3316f444
Madez <mafe.mendez.m@gmail.com>
2021-01-19 23:07:04 -0500

    Connect to database, config mysql connection

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,5 +1,19 @@
 const { Router } = require('express')
-const express = require('express')
+const express = require('express');
+const mysqlConnection = require('../database');
 const router = express.Router();
 
-module.export = router;
+const mysql = require('../database');
+
+// get table employees
+router.get('/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM employees', (err, row, fields) => {
+        if(!err) {
+            res.json(row);
+        } else {
+            console.log(err)
+        }
+    });
+});
+
+module.exports = router;
