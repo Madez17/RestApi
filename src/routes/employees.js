172ece0e59bb1be43df08c002d734086dbf3bab4
Madez <mafe.mendez.m@gmail.com>
2021-01-20 12:43:27 -0500

    Finished CRUD, create dieferents functions to create update adn delete data from employee table

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -16,4 +16,58 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err, row, fields) => {
+        if(!err) {
+            res.json(row[0]);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+// create new employee
+router.post('/', (req, res) => {
+    const { id, name, salary } = req.body;
+    const query = `
+        CALL employeeAddOrEdit(?, ?, ?);
+    `;
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Employee saved'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+// Update Data table employee
+router.put('/:id', (req, res) => {
+    const { name, salary } = req.body;
+    const { id } = req.params;
+    const query = `CALL employeeAddOrEdit(?, ?, ?)`;
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Employee Updated'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+// Delete Data table employee
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Employee deleted'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
 module.exports = router;
